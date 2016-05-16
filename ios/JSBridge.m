d764a4edc32bcbd40cf6cb578e1aedddf01be374
chajn <chajn@qq.com>
2016-05-16 16:20:41 +0800

    Update JSBridge.m

--- ios/JSBridge.m
+++ ios/JSBridge.m
@@ -7,24 +7,51 @@
 //
 
 #import "JSBridge.h"
-
+//#import "MBProgressHUD.h"
 @implementation JSBridge
 
+@synthesize webView;
+-(void)setWebView:(UIView *)webView
+{
+    webView = (UIView *) webView;
+}
 
+-(UIView *)webView
+{
+    if(IOS8){
+        return _wkwebview;
+    }else{
+        return _uiwebview;
+    }
+}
 
 - (instancetype)initWithFrame: (CGRect)frame
 {
+
+    if(!IOS8){
+        UIWebView *uiwebview = [[UIWebView alloc] initWithFrame:frame];
+        self.uiwebview = uiwebview;
+        [self setWebView: uiwebview];
+        return self;
+    }
     
     if (self == [super init]) {
         
         WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
         [config.userContentController addScriptMessageHandler:self name:@"WebViewJavascriptBridge"];
         
-        WKWebView *webView = [[WKWebView alloc] initWithFrame:frame configuration:config];
-        [webView setBackgroundColor:[UIColor clearColor]];
-        [webView setOpaque:NO];
+        WKWebView *wkwv = [[WKWebView alloc] initWithFrame:frame configuration:config];
+        self.wkwebview = wkwv;
+
+        [wkwv setBackgroundColor:[UIColor clearColor]];
+        [wkwv setOpaque:NO];
         
-        [self setWebView: webView];
+        wkwv.navigationDelegate = self;
+        wkwv.scrollView.delegate = self;
+        wkwv.scrollView.bounces = NO;
+        [wkwv.scrollView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
+        
+        [self setWebView: wkwv];
         [self setHandlerList: [[NSMutableDictionary alloc]init]];
         [self checkCachePath];
     }
@@ -32,69 +59,140 @@
     return self;
 }
 
-- (void)loadPage:(NSString *)pageName viewBg:(NSString *) bg initJS:(NSString *)initJS {
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
     
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
-    NSString *cachesDir = [paths objectAtIndex:0];
-    NSString *dirPath = [cachesDir stringByAppendingPathComponent:@"www"];
-    
-    NSString *fileName = [NSString stringWithFormat:@"/%@.html", pageName];
-    NSString *filePath = [dirPath stringByAppendingPathComponent: fileName];
+    DLog(@"chage = %@",change);
+}
 
-    [self.webView.configuration.userContentController removeAllUserScripts];
+- (void)loadUrl:(NSString *)url initJS:(NSString *)initJS {
+    if(!IOS8){
+        [self loadPage:@"" initJS:nil];
+        return;
+    }
+    
+    if(![AFNetworkReachabilityManager sharedManager].reachable){
+        [self loadPage:@"offline" initJS:[NSString stringWithFormat:@"NativeReady({reload:1,url:'%@',local:0})",url]];
+        return;
+    }
+    
+    [self.wkwebview.configuration.userContentController removeAllUserScripts];
     
     WKUserScript *script = [[WKUserScript alloc] initWithSource:@"(function(){window.WebViewJavascriptBridge={callbackIdList:{},callHandler:function(a,d,e){var b={handlerName:a,data:d};if(e){var c='bridage'+Math.ceil(Math.random()*1000000000);this.callbackIdList[c]=e;b.callbackId=c}window.webkit.messageHandlers.WebViewJavascriptBridge.postMessage(b)},registerHandler:function(a,b){this.callbackIdList[a]=b},execCallback:function(a){if(a&&a.callbackId){this.callbackIdList[a.callbackId]&&this.callbackIdList[a.callbackId](a.data);if(a.callbackId.indexOf('bridage')!=-1){delete this.callbackIdList[a.callbackId]}}}};var ev = document.createEvent('HTMLEvents');ev.initEvent('WebViewJavascriptBridgeReady', true, true);document.dispatchEvent(ev);})(); " injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES];
-
-    [self.webView.configuration.userContentController addUserScript:script];
+    
+    [self.wkwebview.configuration.userContentController addUserScript:script];
     
     if(initJS){
         WKUserScript *initScript = [[WKUserScript alloc] initWithSource:initJS injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
-        [self.webView.configuration.userContentController addUserScript:initScript];
+        [self.wkwebview.configuration.userContentController addUserScript:initScript];
     }
     
-    if(bg){
-        [self.webView setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed: [dirPath stringByAppendingPathComponent:bg]]]];
-    }else{
-//        [self.webView setBackgroundColor: UIColorFromRGB(0xf8f8f8)];
+    [self.wkwebview setBackgroundColor: UIColorFromRGB(0xf8f8f8)];
+    
+    [self.wkwebview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
+    
+    [self showLoadingInView];
+}
+
+- (void)loadPage:(NSString *)pageName initData:(NSDictionary *)dicData {
+    NSMutableDictionary *mutData = [NSMutableDictionary dictionaryWithDictionary:dicData];
+    
+    mutData[@"chn"] = @"appstore";
+    mutData[@"pageName"] = pageName;
+    
+    int online = [AFNetworkReachabilityManager sharedManager].reachable;
+    
+    NSData *JSONData= [NSJSONSerialization dataWithJSONObject:mutData options:NSJSONWritingPrettyPrinted error:NULL];
+    
+    NSString *js = [NSString stringWithFormat:@"nativeReady(%@)",[[NSString alloc]initWithData:JSONData encoding:NSUTF8StringEncoding]];
+    NSString *local =  mutData[@"local"];
+    if(!online && !local){
+        [self loadPage:@"offline" initJS:js];
+        return;
     }
     
+    [self loadPage:pageName initJS:js];
+}
+
+
+- (void)loadPage:(NSString *)pageName initJS:(NSString *)initJS {
+    
+    if(!IOS8){
+        NSString *dirPath = [[NSBundle mainBundle] pathForResource:@"www" ofType:@"bundle"];
+        NSString *itemPath = [dirPath stringByAppendingPathComponent:@"empty.html"];
+        
+        NSURL* url = [NSURL fileURLWithPath: itemPath];//创建URL
+        NSURLRequest* request = [NSURLRequest requestWithURL:url];//创建NSURLRequest
+        [self.uiwebview loadRequest:request];
+        return;
+    }
+    
+    NSString *dirPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"www"];
+    
+    NSString *fileName = [NSString stringWithFormat:@"/%@.html", pageName];
+    NSString *filePath = [dirPath stringByAppendingPathComponent: fileName];
+    
+    
+    [self.wkwebview.configuration.userContentController removeAllUserScripts];
+    
+    WKUserScript *script = [[WKUserScript alloc] initWithSource:@"(function(){window.WebViewJavascriptBridge={callbackIdList:{},callHandler:function(a,d,e){var b={handlerName:a,data:d};if(e){var c='bridage'+Math.ceil(Math.random()*1000000000);this.callbackIdList[c]=e;b.callbackId=c}window.webkit.messageHandlers.WebViewJavascriptBridge.postMessage(b)},registerHandler:function(a,b){this.callbackIdList[a]=b},execCallback:function(a){if(a&&a.callbackId){this.callbackIdList[a.callbackId]&&this.callbackIdList[a.callbackId](a.data);if(a.callbackId.indexOf('bridage')!=-1){delete this.callbackIdList[a.callbackId]}}}};var ev = document.createEvent('HTMLEvents');ev.initEvent('WebViewJavascriptBridgeReady', true, true);document.dispatchEvent(ev);})(); " injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES];
+    
+    [self.wkwebview.configuration.userContentController addUserScript:script];
+    
+    if(initJS){
+        WKUserScript *initScript = [[WKUserScript alloc] initWithSource:initJS injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
+        [self.wkwebview.configuration.userContentController addUserScript:initScript];
+    }
+
+    [self.wkwebview setBackgroundColor: UIColorFromRGB(0xf8f8f8)];
+
+    self.isContentReady = NO;
+
     NSURL* filePathUrl = [NSURL fileURLWithPath:filePath];
     SEL sel = NSSelectorFromString(@"loadFileURL:allowingReadAccessToURL:");
-    if ([self.webView respondsToSelector:sel]) {
-        [self.webView loadFileURL:filePathUrl allowingReadAccessToURL:[NSURL fileURLWithPath:dirPath]];
+    if ([self.wkwebview respondsToSelector:sel]) {
+        [self.wkwebview loadFileURL:filePathUrl allowingReadAccessToURL:[NSURL fileURLWithPath:dirPath]];
     } else {
-        [self.webView loadRequest:[NSURLRequest requestWithURL:filePathUrl]];
+        [self.wkwebview loadRequest:[NSURLRequest requestWithURL:filePathUrl]];
+    }
+    if(![pageName  isEqual: @"weight_record"] &&
+       ![pageName  isEqual: @"bfr_record"]){
+        [self showLoadingInView];
     }
 }
 
 - (BOOL)checkCachePath {
-
+    
     NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
     NSString *currentVersion = [infoDict objectForKey:@"CFBundleVersion"];
     NSString *version = [[NSUserDefaults standardUserDefaults] objectForKey:@"h5wwwVersion"];
     
     NSFileManager *manager = [NSFileManager defaultManager];
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
-    NSString *cachesDir = [paths objectAtIndex:0];
-    NSString *tempPath = [cachesDir stringByAppendingPathComponent:@"www"];
+    NSString *tempPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"www"];
     NSError *error = nil;
     
-    // 正式环境中最好把这段注释打开 除非没找到临时目录或版本号不一致 其他情况不再拷贝文件到临时目录
-//    if([manager fileExistsAtPath:tempPath] && [version isEqualToString:currentVersion]){
-//        return YES;
-//    }else{
-//        if (![manager createDirectoryAtPath:tempPath withIntermediateDirectories:YES attributes:nil error:&error]) {
-//            NSLog(@"Could not create www directory. Error: %@", error);
-//            return NO;
-//        }
-//    }
-    
+
+#ifdef DEBUG
+    //测试环境每次覆盖
+    if (![manager createDirectoryAtPath:tempPath withIntermediateDirectories:YES attributes:nil error:&error]) {
+        NSLog(@"Could not create www directory. Error: %@", error);
+        return NO;
+    }
+#else
+    if(![manager fileExistsAtPath:tempPath]){
+        if (![manager createDirectoryAtPath:tempPath withIntermediateDirectories:YES attributes:nil error:&error]) {
+            NSLog(@"Could not create www directory. Error: %@", error);
+            return NO;
+        }
+    }else if([version isEqualToString:currentVersion]){
+        return YES;
+    }
+#endif
     
     [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:@"h5wwwVersion"];
     
     NSString *dirPath = [[NSBundle mainBundle] pathForResource:@"www" ofType:@"bundle"];
     NSArray* array = [manager contentsOfDirectoryAtPath: dirPath error:&error];
-
+    
     [array enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         NSError *error = nil;
         NSString *itemPath = [dirPath stringByAppendingPathComponent:obj];
@@ -109,12 +207,15 @@
 }
 
 - (void)execJS:(NSString *)funcName jsStr:(NSString *)jsStr isBridage:(BOOL)isBridage {
-    
+    if(!IOS8){
+        return;
+    }
+
     NSString *namespace = isBridage ? @"window.WebViewJavascriptBridge":@"window";
     
     NSString *js = [NSString stringWithFormat:@"(function(){%@['%@'](%@);})();", namespace, funcName, jsStr];
     NSLog(@"%@", js);
-    [self.webView evaluateJavaScript:js completionHandler:^(id val,NSError *error){
+    [self.wkwebview evaluateJavaScript:js completionHandler:^(id val,NSError *error){
         NSLog(@"Completion: %@ : %@", val, error);
     }];
     
@@ -143,14 +244,12 @@
     }
     
     if (!handlerName) {
-        NSLog(@"JSBridge: ERROR: need handlerName");
         return responseCallback(@"{errno:-1,msg:'need handlerName'}");
     }
     
     JSBridgeHandler handler = [self.handlerList objectForKey:handlerName];
     
     if (!handler) {
-        NSLog(@"JSBridge: ERROR: not Object-C handler %@", handlerName);
         return responseCallback(@"{errno:-2, msg:'not Object-C handler'}");
     }
     
@@ -158,9 +257,7 @@
         handler(data, responseCallback);
     }
     @catch (NSException *exception) {
-        NSLog(@"JSBridge: WARNING: objc handler threw. %@ %@", message, exception);
         NSString *msg = [NSString stringWithFormat:@"{errno:-3, msg:'%@'}", message];
-        
         return responseCallback(msg);
     }
     
@@ -177,6 +274,12 @@
     [self execJS:@"execCallback" jsStr:jsonData isBridage: YES];
 }
 
+-(void) registerWebComp:(UIViewController *) weakView{
+    [self registerHandler:@"contentReady" handler:^(id data, JSBridgeResponseCallback response) {
+        self.isContentReady = YES;
+    }];
+}
+
 
 -(void)registerHandler: (NSString *)handlerName handler:(JSBridgeHandler) handler {
     [self.handlerList setObject:handler forKey:handlerName];
@@ -188,11 +291,51 @@
 
 - (void)callHandler:(NSString *)handlerName jsonData:(NSString *)jsonData {
     [self execBridge:handlerName jsonData:jsonData];
-//    [self callHandler:handlerName jsonData:jsonData responseCallback:nil];
+    //    [self callHandler:handlerName jsonData:jsonData responseCallback:nil];
 }
 
 //- (void)callHandler:(NSString *)handlerName jsonData:(NSString *)jsonData responseCallback:(JSBridgeResponseCallback)responseCallback {
 //    [self execBridge:handlerName jsonData:jsonData responseCallback:responseCallback];
 //}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
+{
+    [self hideLoadingInView];
+}
+
+/*!
+ *  显示正在加载框
+ */
+- (void)showLoadingInView
+{
+    if (!IOS8) {
+        return;
+    }
+    
+    if(self.hud) return;
+//    self.hud = [MBProgressHUD showHUDAddedTo:self.webView animated:YES];
+//    self.hud.labelText = @"正在加载";
+}
+
+/*!
+ *  隐藏正在加载框
+ */
+- (void)hideLoadingInView
+{
+    if (self.hud) {
+        
+//        [self.hud hide:YES];
+        self.hud = nil;
+    }
+    
+    
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    if (self.bridgeScrollBlock) {
+        self.bridgeScrollBlock(scrollView);
+    }
+}
 
 @end
